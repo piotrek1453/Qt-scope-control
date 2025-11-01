4a2e7ab6cea40b8e44d379e11221091633240b92
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-11-01 22:33:01 +0100

    remove BUILD_SHARED_LIBS=OFF from build script

--- modules/build_and_run.sh
+++ modules/build_and_run.sh
@@ -7,7 +7,7 @@ mkdir -p build
 # change directory to build
 cd build
 # generate files for build type Release with shared libs off
-cmake -GNinja -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release ..
 # build the project
 cmake --build .
 # run the executable
